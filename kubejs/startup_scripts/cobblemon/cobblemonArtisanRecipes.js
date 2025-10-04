c77a0268cd042160498d8b48a6c15f4fbacd8c99
Chakyl <chakyl1127@gmail.com>
2025-10-04 12:29:33 -0400

    seed maker

---
+++ kubejs/startup_scripts/cobblemon/cobblemonArtisanRecipes.js
@@ -0,0 +1,18 @@
+[
+  { input: "cobblemon:red_apricorn", output: ["6x cobblemon:red_apricorn_seed"] },
+  { input: "cobblemon:yellow_apricorn", output: ["6x cobblemon:yellow_apricorn_seed"] },
+  { input: "cobblemon:green_apricorn", output: ["6x cobblemon:green_apricorn_seed"] },
+  { input: "cobblemon:blue_apricorn", output: ["6x cobblemon:blue_apricorn_seed"] },
+  { input: "cobblemon:pink_apricorn", output: ["6x cobblemon:pink_apricorn_seed"] },
+  { input: "cobblemon:black_apricorn", output: ["6x cobblemon:black_apricorn_seed"] },
+  { input: "cobblemon:white_apricorn", output: ["6x cobblemon:white_apricorn_seed"] },
+  { input: "cobblemon:red_mint_leaf", output: ["6x cobblemon:red_mint_seeds"] },
+  { input: "cobblemon:blue_mint_leaf", output: ["6x cobblemon:blue_mint_seeds"] },
+  { input: "cobblemon:cyan_mint_leaf", output: ["6x cobblemon:cyan_mint_seeds"] },
+  { input: "cobblemon:pink_mint_leaf", output: ["6x cobblemon:pink_mint_seeds"] },
+  { input: "cobblemon:green_mint_leaf", output: ["6x cobblemon:green_mint_seeds"] },
+  { input: "cobblemon:white_mint_leaf", output: ["6x cobblemon:white_mint_seeds"] },
+  { input: "cobblemon:vivichoke", output: ["6x cobblemon:vivichoke_seeds"] },
+].forEach((recipe) => {
+  global.seedMakerRecipes.push(recipe);
+});
