30582fc39ce6cecb5e6289411c0f157fb3d39adb
Chakyl <chakyl1127@gmail.com>
2025-10-30 18:31:36 -0400

    fix plushie quality

--- kubejs/client_scripts/jadeClient.js
+++ kubejs/client_scripts/jadeClient.js
@@ -9,17 +9,20 @@ const Vec2 = Java.loadClass("net.minecraft.world.phys.Vec2");
 // TOOD: FIX
 global["JadePlushieClientCallback"] = (tooltip, accessor, pluginConfig) => {
   const nbt = accessor.getServerData();
+
   if (nbt.type.equals("")) return;
-  const type = nbt.type;
+  const type = nbt.type
   let typeData = global.plushieTraits[type];
+  const affection = Number(nbt.affection)
+  const quality = Number(nbt.quality)
   let blockName = accessor.getBlock().getDescriptionId();
   tooltip.clear();
   tooltip.add(Component.translatable(blockName));
-  tooltip.add(`§6${"★".repeat(nbt.quality + 1)}§8${"☆".repeat(3 - nbt.quality)}`);
+  tooltip.add(`§6${"★".repeat(quality + 1)}§8${"☆".repeat(3 - quality)}`);
   tooltip.add(`§${typeData.color}${global.formatName(typeData.trait)}`);
   tooltip.add(
-    `§c${nbt.affection > 0 ? `❤`.repeat(nbt.affection) : ""}§8${
-      nbt.affection < 4 ? `❤`.repeat(4 - nbt.affection) : ""
+    `§c${affection > 0 ? `❤`.repeat(affection) : ""}§8${
+      affection < 4 ? `❤`.repeat(4 - affection) : ""
     }`
   );
 };
