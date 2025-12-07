39dc22a37bba5387d437212d20e285421ec76229
Hexagreen <greentree329@naver.com>
2025-12-08 00:21:30 +0900

    Update addFishTooltips to use Text.translatable instead of Text.translate

--- kubejs/client_scripts/tooltips/addFishTooltips.js
+++ kubejs/client_scripts/tooltips/addFishTooltips.js
@@ -5,7 +5,7 @@ ItemEvents.tooltip((tooltip) => {
   let autumnFish = [];
   let winterFish = [];
 
-  tooltip.add(yearRound, [Text.of(" "), Text.translate("desc.sereneseasons.year_round").lightPurple()]);
+  tooltip.add(yearRound, [Text.of(" "), Text.translatable("desc.sereneseasons.year_round").lightPurple()]);
   const addSeasonTooltip = (item, seasonText, seasonArray) => {
     if (!yearRound.includes(item) && !seasonArray.includes(item)) {
       seasonArray.push(item);
@@ -13,42 +13,42 @@ ItemEvents.tooltip((tooltip) => {
     }
   };
   global.springOcean.forEach((entry) =>
-    addSeasonTooltip(entry.fish, Text.translate("desc.sereneseasons.spring").green(), springFish)
+    addSeasonTooltip(entry.fish, Text.translatable("desc.sereneseasons.spring").green(), springFish)
   );
   global.springRiver.forEach((entry) =>
-    addSeasonTooltip(entry.fish, Text.translate("desc.sereneseasons.spring").green(), springFish)
+    addSeasonTooltip(entry.fish, Text.translatable("desc.sereneseasons.spring").green(), springFish)
   );
   global.springFresh.forEach((entry) =>
-    addSeasonTooltip(entry.fish, Text.translate("desc.sereneseasons.spring").green(), springFish)
+    addSeasonTooltip(entry.fish, Text.translatable("desc.sereneseasons.spring").green(), springFish)
   );
 
   global.summerOcean.forEach((entry) =>
-    addSeasonTooltip(entry.fish, Text.translate("desc.sereneseasons.summer").yellow(), summerFish)
+    addSeasonTooltip(entry.fish, Text.translatable("desc.sereneseasons.summer").yellow(), summerFish)
   );
   global.summerRiver.forEach((entry) =>
-    addSeasonTooltip(entry.fish, Text.translate("desc.sereneseasons.summer").yellow(), summerFish)
+    addSeasonTooltip(entry.fish, Text.translatable("desc.sereneseasons.summer").yellow(), summerFish)
   );
   global.summerFresh.forEach((entry) =>
-    addSeasonTooltip(entry.fish, Text.translate("desc.sereneseasons.summer").yellow(), summerFish)
+    addSeasonTooltip(entry.fish, Text.translatable("desc.sereneseasons.summer").yellow(), summerFish)
   );
 
   global.autumnOcean.forEach((entry) =>
-    addSeasonTooltip(entry.fish, Text.translate("desc.sereneseasons.autumn").gold(), autumnFish)
+    addSeasonTooltip(entry.fish, Text.translatable("desc.sereneseasons.autumn").gold(), autumnFish)
   );
   global.autumnRiver.forEach((entry) =>
-    addSeasonTooltip(entry.fish, Text.translate("desc.sereneseasons.autumn").gold(), autumnFish)
+    addSeasonTooltip(entry.fish, Text.translatable("desc.sereneseasons.autumn").gold(), autumnFish)
   );
   global.autumnFresh.forEach((entry) =>
-    addSeasonTooltip(entry.fish, Text.translate("desc.sereneseasons.autumn").gold(), autumnFish)
+    addSeasonTooltip(entry.fish, Text.translatable("desc.sereneseasons.autumn").gold(), autumnFish)
   );
 
   global.winterOcean.forEach((entry) =>
-    addSeasonTooltip(entry.fish, Text.translate("desc.sereneseasons.winter").aqua(), winterFish)
+    addSeasonTooltip(entry.fish, Text.translatable("desc.sereneseasons.winter").aqua(), winterFish)
   );
   global.winterRiver.forEach((entry) =>
-    addSeasonTooltip(entry.fish, Text.translate("desc.sereneseasons.winter").aqua(), winterFish)
+    addSeasonTooltip(entry.fish, Text.translatable("desc.sereneseasons.winter").aqua(), winterFish)
   );
   global.winterFresh.forEach((entry) =>
-    addSeasonTooltip(entry.fish, Text.translate("desc.sereneseasons.winter").aqua(), winterFish)
+    addSeasonTooltip(entry.fish, Text.translatable("desc.sereneseasons.winter").aqua(), winterFish)
   );
 });
