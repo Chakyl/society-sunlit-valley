6d105c5058418311f957734631cb7f9a20ea5ea0
Chakyl <chakyl1127@gmail.com>
2026-06-23 18:51:41 -0400

    dacount

--- kubejs/server_scripts/playerEvents/dayCounterTick.js
+++ kubejs/server_scripts/playerEvents/dayCounterTick.js
@@ -48,7 +48,7 @@ PlayerEvents.tick((e) => {
     // Sleeping cuts the amount of possible days by half
     let yearCount =
       player.stats.playTime / dayTickDuration / (((global.subSeasonDuration * 3) * 4) / 2);
-    let overrideCount = player.stats.playTime / dayTickDuration / (((10 * 3) * 4) / 2);
+    let overrideCount = player.stats.playTime / dayTickDuration / (((30 * 3) * 4) / 2);
     if (!player.stages.has("master_cultivator_unlocked") &&
       overrideCount > 1 || (yearCount > 1 && ["spring", "summer"].includes(global.getSeasonFromLevel(level)))
     ) {
