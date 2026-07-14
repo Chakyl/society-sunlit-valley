649602e307516c86d14a9971cd5c17dd612e560c
Emma <meeprdib@outlook.com>
2026-07-13 23:30:10 -0400

    barrens edited surface rule, attempted vegetation for biome, added spawn to lush cavern

--- kubejs/server_scripts/tags/editMiscTags.js
+++ kubejs/server_scripts/tags/editMiscTags.js
@@ -1,3 +1,7 @@
 ServerEvents.tags('item', event => {
     event.add('oreganized:stone_types/glance', 'oreganized:glance')
 })
+
+ServerEvents.tags('block', event => {
+    event.add('c:end_stones', 'society:skull_end_stone')
+})
