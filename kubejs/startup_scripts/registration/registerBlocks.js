41e5ef7a98e9d3d6971b0ec929468ed074d77533
Emma <meeprdib@outlook.com>
2026-07-17 18:44:26 -0400

    Lush Caverns better surface rules, port skullstone creator

--- kubejs/startup_scripts/registration/registerBlocks.js
+++ kubejs/startup_scripts/registration/registerBlocks.js
@@ -1,5 +1,26 @@
 StartupEvents.registry("block", (event) => {
 
+    const createSkullVariant = (type, path) => {
+        event.create(`society:skull_${type}`)
+            .soundType("stone")
+            .resistance(3600000)
+            .requiresTool(true)
+            .texture(path)
+            .unbreakable()
+            .tagBlock("society:skull_block");
+    };
+    createSkullVariant("arid_sandstone", "atmospheric:block/arid_sandstone_top");
+    createSkullVariant("blackstone", "minecraft:block/blackstone");
+    createSkullVariant("crimsite", "create:block/palettes/stone_types/natural/crimsite_0")
+    createSkullVariant("end_stone", "minecraft:block/end_stone");
+    createSkullVariant("granite", "minecraft:block/granite")
+    createSkullVariant("limestone", "create:block/block/palettes/stone_types/limestone")
+    createSkullVariant("permafrost", "quark:block/permafrost");
+    createSkullVariant("sandstone", "minecraft:block/sandstone_top");
+    createSkullVariant("scoria", "create:block/palettes/stone_types/scoria")
+    createSkullVariant("stone", "minecraft:block/stone");
+    createSkullVariant("veridium", "create:block/palettes/stone_types/natural/veridium_0")
+
     // Placeholder stuff for SK
     event.create('extractinator:slush')
     event.create('minecraft:grass')
@@ -21,13 +42,7 @@ StartupEvents.registry("block", (event) => {
     event.create('society:omni_geode_node')
     event.create('society:palm_supply_crate')
     event.create('society:sandstone_boulder')
-    event.create('society:skull_arid_sandstone').texture("atmospheric:block/arid_sandstone_top")
-    event.create('society:skull_blackstone').texture("minecraft:block/blackstone")
     event.create('society:skull_cavern_teleporter')
-    event.create('society:skull_end_stone').texture("minecraft:block/end_stone")
-    event.create('society:skull_permafrost').texture("quark:block/permafrost")
-    event.create('society:skull_sandstone').texture("minecraft:block/sandstone_top")
-    event.create('society:skull_stone').texture("minecraft:block/stone")
     event.create('society:sparkstone_ore')
     event.create('society:spruce_supply_crate')
     event.create('society:stone_boulder')
