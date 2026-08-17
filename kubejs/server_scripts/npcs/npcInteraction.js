6d7d1b80d59ea5d3166a81df2d08dab59093030f
Thirace <thirace446@gmail.com>
2026-08-17 18:51:23 +0800

    Gifts

--- kubejs/server_scripts/npcs/npcInteraction.js
+++ kubejs/server_scripts/npcs/npcInteraction.js
@@ -14,8 +14,8 @@ const dialogLengths = {
 }
 const maxGifts = {
     banker: "society:slouching_towards_artistry",
-    blacksmith: Item.of('justhammers:iron_hammer', "{Damage:0,RepairCost:0,display:{Name:'{\"text\":\"§6Aiden\\'s Hammer\"}'}}").enchant('minecraft:efficiency', 5).enchant('minecraft:fortune', 3),
-    carpenter: Item.of('portable_blueprints:worn_blueprint', '{Damage:1,allow_nbt:1,altezza:0,blueprint_name:"blockapedia",buildAnyway:0b,display:{Name:\'{"italic":false,"color":"#FFFF00","text":"Blueprint: Blockapedia"}\'},free_build:1,inventari_blocco_selezionati:"",lunghezzaX:0,lunghezzaZ:0,mirrowX:0b,mirrowY:0b,mirrowZ:0b,nome:"blockapedia",owner:"worn",owner_name:"Ace (Built by Mimsy)",remaining_uses:1,rotateValue:0s,skipObstructionBlock:0b,visualizeBuild:1b,wasHolding:1b,worn_set:1b}'),
+    blacksmith: Item.of('justhammers:iron_hammer', "{Damage:0,RepairCost:0,display:{Name:'{\"translate\":\"item.justhammers.aidens_hammer\"}'}}").enchant('minecraft:efficiency', 5).enchant('minecraft:fortune', 3),
+    carpenter: Item.of('portable_blueprints:worn_blueprint', '{Damage:1,allow_nbt:1,altezza:0,blueprint_name:"blockapedia",buildAnyway:0b,display:{Lore:[\'{"italic":false,"color":"#FFFF00","translate":"portable_blueprints.worn_blueprint.blockapedia.author"}\'],Name:\'{"italic":false,"color":"#FFFF00","translate":"portable_blueprints.worn_blueprint.blockapedia"}\'},free_build:1,inventari_blocco_selezionati:"",lunghezzaX:0,lunghezzaZ:0,mirrowX:0b,mirrowY:0b,mirrowZ:0b,nome:"blockapedia",originPos:"x=-22,y=-60,z=-47",owner:"worn",owner_name:"Ace",remaining_uses:1,rotateValue:0s,skipObstructionBlock:0b,visualizeBuild:0b,wasHolding:1b,worn_set:1b}'),
     fisher: "society:heart_of_neptunium",
     market: "society:universal_methods_of_farming",
     shepherd: Item.of('2x wildernature:penguin_spawn_egg'),
